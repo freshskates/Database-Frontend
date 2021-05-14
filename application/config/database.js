a6a22f33f4d87a413d3dc37ee30219e1ef3cb200
freshskates <business.robertcacho@gmail.com>
2021-05-13 19:58:01 -0700

    fix db bug

--- application/config/database.js
+++ application/config/database.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  // connectionLimit: 50,
-  host: "18.116.158.31",
-  user: "teamproject",
-  password: "user675!",
-  database: "project",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 const promisePool = pool.promise();
